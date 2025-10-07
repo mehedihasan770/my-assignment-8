e6472dcdc84b006b89af1a1115dd98d1a07f5d8f
mehedihasan770 <mehedihasantrbd@gmail.com>
2025-10-07 23:53:24 +0600

    all apps section added and style done

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -1,8 +1,21 @@
+import Card from "../../Components/card/Card";
+import { useAppsJsonData } from "../../Hoks/DataFetch";
 
 const Apps = () => {
+    const {appsData} = useAppsJsonData()
+
     return (
         <div>
-            i am app
+            <div>
+                
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                {
+                    appsData.map(appData => (
+                        <Card key={appData.id} appData={appData}/>
+                    ))
+                }
+            </div>
         </div>
     );
 };
