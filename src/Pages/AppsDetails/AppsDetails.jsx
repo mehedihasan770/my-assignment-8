67b7723618352350ff74fc59725eee876b986ad3
mehedihasan770 <mehedihasantrbd@gmail.com>
2025-10-08 16:20:26 +0600

    Display complete AppDetails page with ratings chart, full description, and install/download card

--- src/Pages/AppsDetails/AppsDetails.jsx
+++ src/Pages/AppsDetails/AppsDetails.jsx
@@ -5,11 +5,12 @@ import { useAppsJsonData } from '../../Hoks/DataFetch';
 import downimg from '../../assets/icon-downloads.png'
 import ratingimg from '../../assets/icon-ratings.png'
 import reviwimg from '../../assets/icon-review.png'
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const AppsDetails = () => {
     const {appsData} = useAppsJsonData()
     const [appDatas, setAppDatas] = useState([])
-    const {image, title, companyName, size, reviews, ratingAvg, downloads} = appDatas
+    const {image, title, companyName, size, reviews, ratingAvg, downloads, ratings, description} = appDatas
     const {id} = useParams()
 
     const idNum = Number(id)
@@ -30,55 +31,73 @@ const AppsDetails = () => {
     return (
         <div className='max-w-[1700px] mx-auto py-20'>
             <div>
-                <div className='flex justify-center items-center space-x-10'>
-                    <div className='bg-white p-10 w-fit rounded-lg'>
-                        <img src={image} alt="" width={'250px'} />
+                <div className='flex flex-col md:flex-row justify-center items-center space-x-10'>
+                    <div className='bg-white p-10 w-fit mx-auto md:mr-10 rounded-lg'>
+                        <img className='mx-auto w-[250px]' src={image} alt=""/>
                     </div>
                     <div className='w-full'>
                         <div className='border-b-2 border-gray-300'>
-                            <h1 className='font-bold text-[32px]'>{title}</h1>
-                            <p className='text-gray-500 text-[20px] pb-5'>Developed by {companyName}</p>
+                            <h1 className='font-bold text-[32px] text-center md:text-left'>{title}</h1>
+                            <p className='text-gray-500 text-[20px] pb-5 text-center md:text-left'>Developed by {companyName}</p>
                         </div>
-                        <div className='pt-5 flex space-x-20'>
-                            <div>
+                        <div className='pt-5 flex flex-wrap md:flex-row'>
+                            <div className='max-w-fit mx-auto lg:mx-10 md:-ml-0 lg:-ml-0'>
                                 <div>
                                     <img src={downimg} alt="" />
                                 </div>
                                 <div>
                                     <h2 className='text-gray-500 font-semibold'>Downloads</h2>
                                 </div>
-                                <div className='-mt-3'>
-                                    <p className='text-[40px] font-bold'>{downloads}</p>
+                                <div className='md:-mt-2'>
+                                    <p className='md:text-[40px] text-2xl font-bold'>{downloads}</p>
                                 </div>
                             </div>
-                            <div>
+                            <div className='max-w-fit mx-auto lg:mx-10'>
                                 <div>
                                     <img src={ratingimg} alt="" />
                                 </div>
                                 <div>
                                     <h2 className='text-gray-500 font-semibold'>Average Ratings</h2>
                                 </div>
-                                <div className='-mt-3'>
-                                    <p className='text-[40px] font-bold'>{ratingAvg}</p>
+                                <div className='md:-mt-2'>
+                                    <p className='md:text-[40px] text-2xl font-bold'>{ratingAvg}</p>
                                 </div>
                             </div>
-                            <div>
+                            <div className='max-w-fit mx-auto lg:mx-10'>
                                 <div>
                                     <img src={reviwimg} alt="" />
                                 </div>
                                 <div>
                                     <h2 className='text-gray-500 font-semibold'>Total Reviews</h2>
                                 </div>
-                                <div className='-mt-3'>
-                                    <p className='text-[40px] font-bold'>{reviews}</p>
+                                <div className='md:-mt-2'>
+                                    <p className='md:text-[40px] text-2xl font-bold'>{reviews}</p>
                                 </div>
                             </div>
                         </div>
-                        <div>
+                        <div className='w-60 md:w-auto mx-auto mt-5 md:mt-0'>
                             <button className='btn w-60 p-6 border-0 bg-[#00D390] font-semibold text-white text-[17px] mt-3'>Install Now ({size} MB)</button>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className='border-b-2 mt-10 mb-10 border-gray-300'></div>
+            <div className="bg-white rounded-lg shadow">
+                <h1 className='text-2xl font-semibold ml-13 py-5'>Ratings</h1>
+                <ResponsiveContainer width="100%" height={500}>
+                    <BarChart data={ratings} layout="vertical" margin={{ top: 20, right: 40, left: 40, bottom: 20 }} barCategoryGap={15}>
+                        <CartesianGrid strokeDasharray="3 3" stroke='none' />
+                        <XAxis axisLine={false} tickLine={false} padding={{left: 20 }}  type="number" margin={{right: 40}}/>
+                        <YAxis axisLine={false} tickLine={false} padding={{bottom: 20 }} dataKey="name" type="category"/>
+                        <Tooltip />
+                        <Bar dataKey="count" fill="#ff8800" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+            <div className='border-b-2 mt-10 mb-10 border-gray-300'></div>
+            <div>
+                <h1 className='text-2xl font-semibold'>Description</h1>
+                <p className='text-gray-500'>{description}</p>
             </div>
         </div>
     );
