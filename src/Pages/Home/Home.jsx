c4e1e94ba1179299007769353834b3b08956f375
mehedihasan770 <mehedihasantrbd@gmail.com>
2025-10-09 20:27:47 +0600

    updated all image URLs to official i.ibb.co links for stability

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import Banner from "../../Components/Banner/Banner";
 import Cards from "../../Components/Cards/Cards";
+import Loading from "../../Components/Loading/Loading";
+import { useAppsJsonData } from "../../Hoks/DataFetch";
 
 const Home = () => {
+    const {loading} = useAppsJsonData()
+    if(loading){
+        return <Loading/>
+    }
+
     return (
         <div>
             <Banner/>
